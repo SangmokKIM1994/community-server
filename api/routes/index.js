80d0f073f5f33b5717afe4bf44b10f9f183db814
minenam <nhm6947@gmail.com>
2023-02-25 00:54:39 +0900

    feat: Comments 라우팅 추가

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => { res.send("정상적으로 요청되었습니다.") });
+const commentsRouter = require("./comments.route.js");
+
+router.use("/api", [commentsRouter]);
+
+router.get("/", (req, res) => {
+  res.send("정상적으로 요청되었습니다.");
+});
 
 module.exports = router;
