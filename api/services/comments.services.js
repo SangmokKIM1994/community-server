28d84c238eca050d7909281da0887fb9bb5f799e
minenam <nhm6947@gmail.com>
2023-02-25 14:52:04 +0900

    feat: Comment Create API - 게시글 아이디 검증 주석 추가

--- api/services/comments.services.js
+++ api/services/comments.services.js
@@ -6,6 +6,8 @@ module.exports = class CommentsService {
   }
   // 댓글 작성
   createComment = async ({ userId, postId, comment }) => {
+    // postId를 검증하는 예외 처리 필요
+
     const result = await this.commentsRepository.createComment({
       userId,
       postId,
