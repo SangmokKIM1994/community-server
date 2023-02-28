fbbafc3226bd06ca82a29ea7943cb498ab20eeee
minenam <nhm6947@gmail.com>
2023-02-28 13:54:41 +0900

    fix: jwt 설치, 댓글 CUD 토큰 인증 추가

--- api/routes/comments.route.js
+++ api/routes/comments.route.js
@@ -2,18 +2,31 @@ const express = require("express");
 const router = express.Router();
 
 const CommentsController = require("../controllers/comments.controller.js");
+const authMiddleware = require("../middlewares/auth.middleware.js");
 const commentsController = new CommentsController();
 
 // 특정 게시물의 댓글 작성
-router.post("/posts/:postId/comments", commentsController.createComment);
+router.post(
+  "/posts/:postId/comments",
+  authMiddleware,
+  commentsController.createComment
+);
 
 // 특정 게시물의 댓글 목록 조회
 router.get("/posts/:postId/comments", commentsController.getCommentsByPost);
 
 // 특정 게시물의 댓글 수정
-router.patch("/comments/:commentId", commentsController.editComment);
+router.patch(
+  "/comments/:commentId",
+  authMiddleware,
+  commentsController.editComment
+);
 
 // 특정 게시물의 댓글 삭제
-router.delete("/comments/:commentId", commentsController.deleteComment);
+router.delete(
+  "/comments/:commentId",
+  authMiddleware,
+  commentsController.deleteComment
+);
 
 module.exports = router;
