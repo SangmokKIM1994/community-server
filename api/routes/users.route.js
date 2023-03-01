4f11b13ea944959ff9c2d67d70a6c4f920e7a932
Ryeonjoo <syeonjoo@naver.com>
2023-03-02 02:26:50 +0900

    fix: 이메일, 닉네임 중복 검사 작성

--- api/routes/users.route.js
+++ api/routes/users.route.js
@@ -4,6 +4,7 @@ const UsersController = require("../controllers/users.controller");
 const usersController = new UsersController();
 
 router.post("/signup", usersController.createSignup);
+router.get("/check", usersController.duplicateCheck);
 router.post("/login", usersController.createLogin);
 
 module.exports = router;
