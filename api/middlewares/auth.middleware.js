5997a111c5b32e59bff94f4e21021b447d9fc22f
Ryeonjoo <syeonjoo@naver.com>
2023-03-01 17:21:30 +0900

    fix: 비회원에 대한 코드 수정

--- api/middlewares/auth.middleware.js
+++ api/middlewares/auth.middleware.js
@@ -1,24 +1,17 @@
 const jwt = require("jsonwebtoken");
 const { Users } = require("../../db/models");
+const PostsController = require("../controllers/posts.controller");
 
 module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
   const [tokenType, token] = (authorization ?? "").split(" ");
-  if (tokenType !== "Bearer" || !token) {
-    return res.status(401).json({
-      errormessage: "로그인 후에 이용할 수 있는 기능입니다.",
-    });
-  }
 
-  try {
+  if (token) {
     const { userId } = jwt.verify(token, process.env.JWT_KEY);
-    const user = await Users.findOne({ where: { userId } });
-    res.locals.user = user;
 
-    next();
-  } catch (error) {
-    return res.status(401).json({
-      message: "토큰이 일치하지 않습니다.",
-    });
+    const user = await Users.findOne({ where: { userId } });
+
+    res.locals.user = user;
   }
+  next();
 };
