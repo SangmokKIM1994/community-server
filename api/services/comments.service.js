67050f401ea287c57a2b9bc9e97c2f6187b9d36a
minenam <nhm6947@gmail.com>
2023-03-01 15:14:52 +0900

    refactor: 커스텀 에러로 statusCode와 message 전달하도록

--- api/services/comments.service.js
+++ api/services/comments.service.js
@@ -1,3 +1,7 @@
+const {
+  NotFoundError,
+  ForbiddenError,
+} = require("../middlewares/customError.js");
 const CommentsRepository = require("../repositories/comments.repository.js");
 
 module.exports = class CommentsService {
@@ -16,7 +20,7 @@ module.exports = class CommentsService {
     if (!result) {
       throw new Error("댓글 작성에 실패하였습니다.");
     }
-    return { success: true, message: "댓글이 생성되었습니다." };
+    return { message: "댓글이 생성되었습니다." };
   };
 
   // 댓글 목록 조회
@@ -28,7 +32,7 @@ module.exports = class CommentsService {
     if (!comments) {
       throw new Error("댓글 목록 조회에 실패하였습니다.");
     }
-    return { success: true, comments };
+    return { data: comments };
   };
 
   // 댓글 수정
@@ -37,10 +41,11 @@ module.exports = class CommentsService {
       commentId,
     });
     if (!existComment) {
-      throw new Error("댓글 조회에 실패하였습니다.");
+      throw new NotFoundError("댓글 조회에 실패하였습니다.");
+      // throw new Error("댓글 조회에 실패하였습니다.");
     }
-    if (!existComment.userId === userId) {
-      throw new Error("권한이 없습니다.");
+    if (existComment.userId !== userId) {
+      throw new ForbiddenError("권한이 없습니다.");
     }
     const updated = await this.commentsRepository.editComment({
       commentId,
@@ -49,7 +54,7 @@ module.exports = class CommentsService {
     if (!updated) {
       throw new Error("댓글 수정에 실패하였습니다.");
     }
-    return { success: true, message: "댓글이 수정되었습니다." };
+    return { message: "댓글이 수정되었습니다." };
   };
 
   // 댓글 삭제
@@ -58,10 +63,10 @@ module.exports = class CommentsService {
       commentId,
     });
     if (!existComment) {
-      throw new Error("댓글 조회에 실패하였습니다.");
+      throw new NotFoundError("댓글 조회에 실패하였습니다.");
     }
-    if (!existComment.userId === userId) {
-      throw new Error("권한이 없습니다.");
+    if (existComment.userId !== userId) {
+      throw new ForbiddenError("권한이 없습니다.");
     }
     const deleted = await this.commentsRepository.deleteComment({
       commentId,
@@ -69,6 +74,6 @@ module.exports = class CommentsService {
     if (!deleted) {
       throw new Error("댓글 삭제에 실패하였습니다.");
     }
-    return { success: true, message: "댓글이 삭제되었습니다." };
+    return { message: "댓글이 삭제되었습니다." };
   };
 };
